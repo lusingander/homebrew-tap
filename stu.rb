15cbe12123ec9a9a79cffe741b55d3b6ceb38ba8
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-03-17 19:48:30 +0900

    Update stu

--- stu.rb
+++ stu.rb
@@ -1,16 +1,16 @@
 class Stu < Formula
   desc "TUI application for AWS S3 written in Rust using ratatui"
   homepage "https://github.com/lusingander/stu"
-  version "0.3.0"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lusingander/stu/releases/download/v0.3.0/stu-0.3.0-darwin-arm64.tar.gz"
-      sha256 "b1913bd80a373b36a082bcbc9628eada788c7f23b792b7efcd79a93bdcfbdb3d"
+      url "https://github.com/lusingander/stu/releases/download/v0.4.0/stu-0.4.0-darwin-arm64.tar.gz"
+      sha256 "4b2db10ea00374bf3ff52f4bba12939fb4fd6743ac4cc95d718805c297f10eaf"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/lusingander/stu/releases/download/v0.3.0/stu-0.3.0-darwin-amd64.tar.gz"
-      sha256 "4c5c1377a11c83000c3a45ee67593c7cbce7f7c0e4c80e2155b1a9f7d38c2804"
+      url "https://github.com/lusingander/stu/releases/download/v0.4.0/stu-0.4.0-darwin-amd64.tar.gz"
+      sha256 "c26de346d02734ac61d7947c5923e8d7c0b466e71a3085cd4082d8f934dfb355"
     end
   end
 

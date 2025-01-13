ce6c220980132788e2772557379b606c4d6067bb
Kyosuke Fujimoto <kyoro.f@gmail.com>
2025-01-13 09:11:12 +0900

    Update serie

--- serie.rb
+++ serie.rb
@@ -1,16 +1,16 @@
 class Serie < Formula
   desc "A rich git commit graph in your terminal, like magic"
   homepage "https://github.com/lusingander/serie"
-  version "0.4.1"
+  version "0.4.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lusingander/serie/releases/download/v0.4.1/serie-0.4.1-aarch64-apple-darwin.tar.gz"
-      sha256 "cc8c9a646a1d131e4a26d974ee6ff9625ee57cd92954b95d9657ef74281d07af"
+      url "https://github.com/lusingander/serie/releases/download/v0.4.2/serie-0.4.2-aarch64-apple-darwin.tar.gz"
+      sha256 "084829de54136376f7968420d0b6157a0e3da8fd2f3147d8fda5289a4d0153e5"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/lusingander/serie/releases/download/v0.4.1/serie-0.4.1-x86_64-apple-darwin.tar.gz"
-      sha256 "65e2c123cc32723a93a37b81990c42ee008843965cc4e282aee374f75e78b8ab"
+      url "https://github.com/lusingander/serie/releases/download/v0.4.2/serie-0.4.2-x86_64-apple-darwin.tar.gz"
+      sha256 "1f1f4c4520b7474dcbc2a9307c3de898991c86bf88d413a6c4973ae74ea7b736"
     end
   end
 

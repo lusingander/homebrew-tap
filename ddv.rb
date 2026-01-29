8c6d550b0f23c27e711fb5e2260d3e200ea12df6
Kyosuke Fujimoto <kyoro.f@gmail.com>
2026-01-29 12:57:11 +0900

    Update ddv

--- ddv.rb
+++ ddv.rb
@@ -1,16 +1,16 @@
 class Ddv < Formula
   desc "Terminal DynamoDB Viewer"
   homepage "https://github.com/lusingander/ddv"
-  version "0.3.0"
+  version "0.3.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lusingander/ddv/releases/download/v0.3.0/ddv-0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "8e6cd4f8fafdf52279c5c9c10bed3269e9ba044f7b76d6e75b7027ec2ea6833e"
+      url "https://github.com/lusingander/ddv/releases/download/v0.3.1/ddv-0.3.1-aarch64-apple-darwin.tar.gz"
+      sha256 "757104c00146ddba685ed2b63840caa84f5bd1c2c52a743c0255f11bcb7842c5"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/lusingander/ddv/releases/download/v0.3.0/ddv-0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "c43592641b75c69804c25c9bdd463b99eaaaa0978283bc5d8fbff1dce4d618b2"
+      url "https://github.com/lusingander/ddv/releases/download/v0.3.1/ddv-0.3.1-x86_64-apple-darwin.tar.gz"
+      sha256 "feb0cfcbb86a52acd0d44d06632fdd03f661474dd1422f10f8c749753938630b"
     end
   end
 

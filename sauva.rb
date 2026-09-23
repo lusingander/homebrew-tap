a3e3aab35c42c25ac604e342b650ffa3752ef991
Kyosuke Fujimoto <kyoro.f@gmail.com>
2026-09-23 22:25:41 +0900

    Update sauva

--- sauva.rb
+++ sauva.rb
@@ -1,16 +1,16 @@
 class Sauva < Formula
   desc "Terminal Unicode Explorer"
   homepage "https://github.com/lusingander/sauva"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lusingander/sauva/releases/download/v0.1.0/sauva-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "091400649f580b62306c68cd246fcba35bb821bce1e802546f881e4846b9cfa5"
+      url "https://github.com/lusingander/sauva/releases/download/v0.2.0/sauva-0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "bfa5e3f2c07025068ed5035ba4ee5a9623c41d139b694919971a6e9121ce8aab"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/lusingander/sauva/releases/download/v0.1.0/sauva-0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "8ca940debcc2f6706dae940668f817c14f94a429b2824b718eab04c30a065869"
+      url "https://github.com/lusingander/sauva/releases/download/v0.2.0/sauva-0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "08b456b564be77cb584bf3b6d5e14392ada0f31a8681d72d1bd2582aa78bb3a2"
     end
   end
 
